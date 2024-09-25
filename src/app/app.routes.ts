282b241463100e1845bd8a9a148340263e2af3a5
nikusha123456 <nikab6264@gmail.com>
2024-09-25 13:15:52 +0400

    removing about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { AboutComponent } from './pages/about/about.component';
 import { TechStackComponent } from './pages/tech-stack/tech-stack.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
@@ -9,10 +8,6 @@ export const routes: Routes = [
   {
     path: '',
     component: MainContentComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
   },
   {
     path: 'tech-stack',
